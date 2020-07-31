f64c6a97d27eb2fe6793c31a78fef388f2e33415
Joseph <josephpower99@gmail.com>
2020-07-31 19:15:15 -0400

    Testing commit

--- 180639510_181948500_a04.c
+++ 180639510_181948500_a04.c
@@ -10,3 +10,38 @@
  * GitHub: josephpow
  * Student ID: 181948500
 **/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+
+int resourceCount;
+int *resourceAvailable;
+int **max;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Bad input! \n");
+        return -1;
+    }
+    resourceCount = argc - 1;
+    resourceAvailable = malloc(sizeof(int) * resourceCount);
+    for (int i = 0; i < argc; i++)
+    {
+        resourceAvailable[i - 1] = atoi(argv[i]);
+    }
+    /*
+    for (int i = 0; i < resourceCount; i++)
+    {
+        printf("%d, ", resourceAvailable[i]);
+    }
+    */
+    return 0;
+}
